2563ce859ae86c38f68e1070902bf644ed459a36
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-03-15 13:32:46 -0500

    functional component

---
+++ src/Components/ternary-operator.js
@@ -0,0 +1,13 @@
+function TernaryOperator() {
+  return (
+    <div>
+      <h2>ES6 Ternary Operator</h2>
+      <p>
+        The ternary operator is a simplified conditional operator like if /
+        else.
+      </p>
+    </div>
+  );
+}
+
+export default TernaryOperator;
